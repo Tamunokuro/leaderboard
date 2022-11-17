345d34a6ab2d53f35b59558e42ef1f7eabf18065
Tamunokuro <tamunokrobluejack@gmail.com>
2022-11-17 14:28:44 +0100

    Fix linters

--- src/modules/createScore.js
+++ src/modules/createScore.js
@@ -1,16 +1,14 @@
-const postScore = async (name, score) => {
-  return await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xvXWioll8KUnbnQMU2ky/scores', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user: name,
-      score,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => data.result);
-};
+const postScore = async (name, score) => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xvXWioll8KUnbnQMU2ky/scores', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    user: name,
+    score,
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => data.result);
 
 export default postScore;
--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,8 +1,6 @@
-const getScore = async () => {
-  return await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xvXWioll8KUnbnQMU2ky/scores')
-    .then((response) => response.json())
-    .then((data) => data.result)
-    .catch((error) => error);
-};
+const getScore = async () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xvXWioll8KUnbnQMU2ky/scores')
+  .then((response) => response.json())
+  .then((data) => data.result)
+  .catch((error) => error);
 
 export default getScore;
