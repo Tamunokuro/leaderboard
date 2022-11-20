1979d27785ee90c22853fc574a3fe34d561b8409
Tamunokuro <tamunokrobluejack@gmail.com>
2022-11-20 15:00:20 +0100

    Edit Leaderboard UI

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,4 +1,4 @@
-const getScore = async () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xvXWioll8KUnbnQMU2ky/scores')
+const getScore = async () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BJq97vqQyuDrDz6yc9qi/scores')
   .then((response) => response.json())
   .then((data) => data.result)
   .catch((error) => error);
